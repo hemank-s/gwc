010126c425fc6f88d8373b9dbd0c14fa923e759f
hemank-s <hemank@hike.in>
2016-04-26 12:46:37 +0530

    Initiating the Growth Console Dashboard Angular

---
+++ gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                options: {
+                    paths: ["css"]
+                },
+                files: {
+                    "css/app.css": "less/app.less",
+                },
+                cleancss: true
+            }
+        },
+        csssplit: {
+            your_target: {
+                src: ['css/app.css'],
+                dest: 'css/app.min.css',
+                options: {
+                    maxSelectors: 4095,
+                    suffix: '.'
+                }
+            },
+        },
+        ngtemplates: {
+          materialAdmin: {
+            src: ['template/**.html', 'template/**/**.html'],
+            dest: 'js/templates.js',
+            options: {
+              htmlmin: {
+                    collapseWhitespace: true,
+                    collapseBooleanAttributes: true
+              }
+            }
+          }
+        },
+        watch: {
+            a: {
+                files: ['less/**/*.less'], // which files to watch
+                tasks: ['less', 'csssplit'],
+                options: {
+                    nospawn: true
+                }
+            },
+            b: {
+                files: ['template/**/*.html'], // which files to watch
+                tasks: ['ngtemplates'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-csssplit');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-angular-templates');
+
+    grunt.registerTask('default', ['less']);
+
+};
